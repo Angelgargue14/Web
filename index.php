df3db94aaa76760035f0f04b290371872f133958
Ángel García Guerra <angelgargue14@gmail.com>
2025-05-06 09:47:14 +0200

    Paleta de colores 20250506

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Colores.css">
     <link rel="stylesheet" href="css/Fuente.css">
     <link rel="stylesheet" href="css/styleGrid.css">
     <link rel="stylesheet" href="css/Categorias.css">  
